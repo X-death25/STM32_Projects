4307fdb640dd134442a7dc8c87714c176d59c63c
X-death25 <X-death@ultimate-consoles.fr>
2017-05-24 12:11:58 +0200

    add SMS_Dumper

--- SMS_Dumper/stm32/build.sh
+++ SMS_Dumper/stm32/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-make clean
-clear
 make
+rm smsdumper_stm32.bin
+rm smsdumper_stm32.elf
+rm smsdumper_stm32_bootldr.elf
+mv *.bin out
+rm smsdumper_stm32.o
+rm smsdumper_stm32_bootldr.o
 
 
